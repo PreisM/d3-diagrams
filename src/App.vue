ac170aa48f322aceb48db389010279a4105d2971
Maciej Preis <maciej.preis@onet.eu>
2021-03-06 15:17:27 +0100

    circleGraph

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <chart-maker />
+    <circleGraph />
   </div>
 </template>
 
 <script>
 import chartMaker from './components/chartMaker.vue'
+import circleGraph from './components/circleGraph.vue'
 
 export default {
   name: 'App',
   components: {
-    chartMaker
+    chartMaker,
+    circleGraph
   }
 }
 </script>
